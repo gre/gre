1148f2e404459eb632a854fb04ea11d89e626f2f
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2020-10-29 20:18:02 +0100

    One Day One Shader

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,58 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { Surface } from "gl-react-dom";
+import { Shaders, Node, GLSL } from "gl-react";
+
+const shaders = Shaders.create({
+  day001: {
+    frag: GLSL`
+precision highp float;
+varying vec2 uv;
+uniform float blue;
+void main() {
+  gl_FragColor = vec4(uv.x, uv.y, blue, 1.0);
+}`,
+  },
+});
+
+class Day001 extends React.Component {
+  static n = 1;
+  static title = "Hello World";
+  render() {
+    const { blue } = this.props;
+    return <Node shader={shaders.day001} uniforms={{ blue }} />;
+  }
+}
 
 export default function Home() {
+  const Current = Day001;
+  const Day = Day001;
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>One Day One Shader</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <header>
+          <h1 className="title">One Day, One Shader</h1>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+          <p className="description">
+            Day {Day.n}. <span className="daily">"{Day.title}"</span> by{" "}
+            <a href="https://twitter.com/greweb" target="_blank">
+              @greweb
+            </a>
+          </p>
+        </header>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="day">
+          <Surface width={400} height={400}>
+            <Day blue={0.5} />
+          </Surface>
         </div>
-      </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
+        <footer>France confinement #2 Day {Current.n} – Be strong 💪</footer>
+      </main>
 
       <style jsx>{`
         .container {
@@ -70,29 +65,9 @@ export default function Home() {
         }
 
         main {
-          padding: 5rem 0;
           flex: 1;
           display: flex;
           flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
           justify-content: center;
           align-items: center;
         }
@@ -102,21 +77,10 @@ export default function Home() {
           text-decoration: none;
         }
 
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
+        a:hover,
+        a:focus,
+        a:active {
           text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
         }
 
         .title,
@@ -124,69 +88,29 @@ export default function Home() {
           text-align: center;
         }
 
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
+        .title {
           margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
+          line-height: 1.15;
+          font-size: 2rem;
         }
 
-        .logo {
-          height: 1em;
+        .description {
+          line-height: 1rem;
+          font-size: 1.5rem;
+          font-weight: 300;
         }
 
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
+        .description .daily {
+          font-weight: 600;
+        }
+
+        header {
+          padding: 1rem 0;
+        }
+        footer {
+          padding: 1rem 0;
+          font-size: 0.8rem;
+          font-style: italic;
         }
       `}</style>
 
@@ -205,5 +129,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
