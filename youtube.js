47a1a35c484fe15bc56afc88fb132f9b09075896
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2020-11-15 15:45:55 +0100

    fixes time

--- youtube.js
+++ youtube.js
@@ -5,7 +5,7 @@ const episodes = ["7_xNlfX2q9c", "6Xuls7CjQuM"];
 
 const nextYoutubeId = episodes[episodes.length - 1];
 const nextYoutubeTimestamp =
-  firstTimestamp + (episodes.length - 1) * 7 * 60 * 60 * 1000;
+  firstTimestamp + (episodes.length - 1) * 7 * 24 * 60 * 60 * 1000;
 
 // TODO how to fetch this?
 export function useNextYoutube() {
