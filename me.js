895d5c516bae57b9693b89153d9479a05b2c77a0
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2021-05-01 17:08:11 +0200

    me.js

---
+++ me.js
@@ -0,0 +1,46 @@
+export default {
+  title: "greweb.me",
+  description:
+    "Gaëtan Renaudeau (greweb). French developer at Ledger. creative coder experimenting with GLSL shaders, Rust and fountain pens robot plots. infinite noise explorer.",
+  thumbnail: "/profile.jpg",
+  thumbnailDomain: "http://greweb.me",
+  siteDomain: "https://greweb.me",
+  social: [
+    {
+      id: "twitter",
+      url: "https://twitter.com/greweb",
+      icon: "/icons/twitter.svg",
+      text: "@greweb",
+    },
+    {
+      id: "instagram",
+      url: "https://instagram.com/greweb",
+      icon: "/icons/instagram.svg",
+      text: "greweb",
+    },
+    {
+      id: "github",
+      url: "https://github.com/gre",
+      icon: "/icons/github.svg",
+      text: "gre",
+    },
+    {
+      id: "twitch",
+      url: "https://twitch.tv/greweb",
+      icon: "/icons/twitch.svg",
+      text: "greweb",
+    },
+    {
+      id: "itch.io",
+      url: "https://greweb.itch.io",
+      icon: "/icons/iconmonstr-gamepad-3.svg",
+      text: "greweb.itch.io",
+    },
+    {
+      id: "hicetnunc",
+      url: "https://hic.link/greweb",
+      icon: "/icons/hic.svg",
+      text: "hic.link/greweb",
+    },
+  ],
+};
