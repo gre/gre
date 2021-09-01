308c2612be75a970de52beba6b92187c17f4d2d3
@greweb <renaudeau.gaetan@gmail.com>
2021-09-01 08:05:25 +0200

    Update me.js

--- me.js
+++ me.js
@@ -1,7 +1,7 @@
 export default {
   title: "greweb.me",
   description:
-    "Gaëtan Renaudeau (greweb). French developer at Ledger. creative coder experimenting with GLSL shaders, Rust, and fountain pens robot plots. infinite noise explorer.",
+    "creative coder experimenting with GLSL shaders, Rust, and fountain pens robot plots. infinite noise explorer.",
   thumbnail: "/profile.jpg",
   thumbnailDomain: "http://greweb.me",
   siteDomain: "https://greweb.me",
