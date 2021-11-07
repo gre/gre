eb00359920e455f882aa5b6406bb2e348d3937a5
gre <gre@users.noreply.github.com>
2021-11-07 20:27:46 +0000

    deploy: bd3f0e9f0a18258793297f0ac455ea68525a34bb

---
+++ _next/static/DFS_PwF7c_29U8d0kq5L9/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fplots","\u002Fposts","\u002F","\u002F[year]\u002F[month]\u002F[slug]","\u002Fplots\u002F[n]","\u002Fplots\u002Ftags\u002F[tag]","\u002F[year]","\u002Fexport\u002F[day]","\u002Fshaderday\u002F[day]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
