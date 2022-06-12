c466d1bd480018f1bfa77f7a787c54e7865d9e39
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2022-06-12 10:50:05 +0200

    up

---
+++ plots/examples/544/README.md
@@ -0,0 +1,17 @@
+---
+title: "Plottable Circleception (Cover)"
+image: /images/plots/544.gif
+video: /images/plots/544.mp4
+description: "Plot reveal for 'Circleception' plot loop"
+sourceFolderURL: https://github.com/gre/gre/tree/master/doodles/plottable-loop-circleception
+nftGenerator: https://www.fxhash.xyz/generative/14692
+tags:
+  - plotloop
+  - circleception
+---
+
+**This plot shows the physical counterpart of the cover a new generator called "Circleception"**.
+
+The physical is a grid of 4x2 frames that can be used to produce a 8 frames loop video.
+
+'Circleception' explores fractal-like zoom loops (the art infinitely reproduces inside itself), packing circles, filled with various techniques.
--- scripts/perspective-crop-grid-loop.sh
+++ scripts/perspective-crop-grid-loop.sh
@@ -2,7 +2,7 @@
 SCRIPTS=`dirname $0`
 
 # example
-# ./scripts/perspective-crop-grid-loop.sh photo.jpg output_folder 8 640 640 4x2 569,1613 3251,1641 5962,1666 8702,1694 11529,1697 513,4303 3219,4347 5917,4390 8636,4434 11408,4478 375,7023 3130,7059 5861,7105 8597,7157 11366,7222
+# ./scripts/perspective-crop-grid-loop.sh photo.jpg output_folder 8 4x2 569,1613 3251,1641 5962,1666 8702,1694 11529,1697 513,4303 3219,4347 5917,4390 8636,4434 11408,4478 375,7023 3130,7059 5861,7105 8597,7157 11366,7222
 
 set -x
 set -e
@@ -12,8 +12,8 @@ OUTPUT_DIR=$1
 shift
 FPS=$1
 shift
-WIDTH=$1
-HEIGHT=$2
+WIDTH=1280
+HEIGHT=1280
 
 if [ -z "$INPUT" ] || [ -z "$OUTPUT_DIR" ] || [ -z "$FPS" ]; then
   echo "Usage: $0 input outputdir fps width height 4x2 ...list_of_pixel_positions..."
@@ -25,13 +25,18 @@ mkdir -p $OUTPUT_DIR
 
 IFS=$'\n'
 I=0
-for param in `node $SCRIPTS/perspective-crop-grid-loop-make-params.js $*`; do
+for param in `node $SCRIPTS/perspective-crop-grid-loop-make-params.js $WIDTH $HEIGHT $*`; do
     convert $INPUT -matte -virtual-pixel transparent -distort Perspective $param -crop ${WIDTH}x${HEIGHT}+0+0 $OUTPUT_DIR/$I.png
     I=$(($I+1))
 done
 
 ffmpeg -y -r $FPS -i $OUTPUT_DIR/%d.png -c:v libx264 -vf "fps=$FPS,format=yuv420p" $OUTPUT_DIR/out.mp4
 palette=$OUTPUT_DIR/palette.png
+filters="fps=$FPS,scale=$WIDTH:$HEIGHT:flags=lanczos"
+ffmpeg -v warning -i $OUTPUT_DIR/out.mp4 -vf "$filters,palettegen" -y $palette
+ffmpeg -v warning -i $OUTPUT_DIR/out.mp4 -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $OUTPUT_DIR/out-full.gif
+WIDTH=640
+HEIGHT=640
 filters="fps=$FPS,scale=$WIDTH:$HEIGHT:flags=lanczos"
 ffmpeg -v warning -i $OUTPUT_DIR/out.mp4 -vf "$filters,palettegen" -y $palette
 ffmpeg -v warning -i $OUTPUT_DIR/out.mp4 -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $OUTPUT_DIR/out.gif
