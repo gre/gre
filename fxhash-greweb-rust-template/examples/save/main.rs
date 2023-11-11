2a5f0c6ec78b643f92af541a6488a0b39e199301
greweb <greweb@protonmail.com>
2023-11-11 18:56:48 +0100

    template

---
+++ fxhash-greweb-rust-template/examples/save/main.rs
@@ -0,0 +1,16 @@
+use greweb::*;
+use rand::prelude::*;
+
+fn main() {
+  let mut rng = rand::thread_rng();
+  let alphabet: Vec<char> =
+    "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ"
+      .chars()
+      .collect();
+  let chars: String = (0..49)
+    .map(|_i| alphabet[rng.gen_range(0..alphabet.len())])
+    .collect();
+  let hash = format!("oo{}", chars);
+  let code = render(hash, 210.0, 297.0, 10.0, false);
+  std::fs::write("image.svg", code).expect("Unable to write file");
+}
