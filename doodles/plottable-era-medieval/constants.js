732f4d2dea87e76ca98589cd8bd7ad95194a1fa4
greweb <greweb@protonmail.com>
2023-12-11 11:16:57 +0100

    move project

--- doodles/plottable-era-medieval/constants.js
+++
@@ -1,4 +0,0 @@
-export const height = 297;
-export const width = 210;
-export const pad = 10;
-
--- doodles/plottable-era-medieval/index.js
+++ doodles/plottable-era-medieval/index.js
@@ -1,382 +1,276 @@
-// @flow
 /**
  * LICENSE CC BY-NC-ND 4.0
- * Author: greweb – 2023 – Plottable Era (II) Medieval
+ * Author: greweb – 2023 – Plottable Era: (II) Medieval
  */
-import React, { useEffect, useMemo, useState } from "react";
-import { Surface } from "gl-react-dom";
-import { GLSL, LinearCopy, Node, Shaders, Uniform } from "gl-react";
-import Color from "color";
-import init, { render } from "./rust/pkg/main";
-import wasm from "base64-inline-loader!./rust/pkg/main_bg.wasm";
-import { generateVariables, inferProps, inferPalette } from "./variables";
-import { width, height } from "./constants";
+import paperGLSL from "./shaders/paper.glsl";
+import mainGLSL from "./shaders/main.glsl";
+import mainDarkGLSL from "./shaders/main-dark.glsl";
 
-function decode(dataURI) {
-  const binaryString = atob(dataURI.split(",")[1]);
-  var bytes = new Uint8Array(binaryString.length);
-  for (var i = 0; i < binaryString.length; i++) {
-    bytes[i] = binaryString.charCodeAt(i);
+Promise.all([
+  import("./pkg"),
+  fetch("./PrinceValiant.ttf").then(r => r.arrayBuffer()).then(r => new Uint8Array(r)),
+]).then(([rust, fontdata]) => {
+  const hash = $fx.hash;
+  const width = 210;
+  const height = 297;
+  const pad = 5;
+  const precision = 0.2;
+  const debug = new URLSearchParams(window.location.search).has("debug");
+
+  // Generate the SVG
+
+  const prev = Date.now();
+  const svg = rust.render(hash, width, height, pad, precision, fontdata, true, debug);
+  console.log("generated in " + (Date.now() - prev) + "ms");
+
+  if (debug) {
+    const perf = JSON.parse(svg.match("data-perf='([^']+)'")[1]);
+    console.table(perf.per_label);
   }
-  return bytes.buffer;
-}
-let wasmLoaded = false;
-const promiseOfLoad = init(decode(wasm)).then(() => {
-  wasmLoaded = true;
-});
 
-const svgSize = [width, height];
-const MAX = 4096;
-const ratio = svgSize[0] / svgSize[1];
-const svgMMSize = svgSize.map((s) => s + "mm");
+  const palette = JSON.parse(svg.match("data-palette='([^']+)'")[1]);
 
-let adaptiveSvgWidth = (width) => Math.max(64, Math.ceil(width / 64) * 64);
-
-const Main = ({ width, height, setFeatures, hash }) => {
-  const dpr = window.devicePixelRatio || 1;
-  let W = width;
-  let H = height;
-  H = Math.min(H, W / ratio);
-  W = Math.min(W, H * ratio);
-  W = Math.floor(W);
-  H = Math.floor(H);
-  let w = Math.min(MAX, dpr * W);
-  let h = Math.min(MAX, dpr * H);
-  h = Math.min(h, w / ratio);
-  w = Math.min(w, h * ratio);
-  w = Math.floor(w);
-  h = Math.floor(h);
-  const svgW = adaptiveSvgWidth(w);
-  const widthPx = svgW + "px";
-  const heightPx = Math.floor(svgW / ratio) + "px";
-
-  const [loaded, setLoaded] = useState(wasmLoaded);
-  const variables = useVariables({ hash, });
-
-  useEffect(() => {
-    if (!loaded) promiseOfLoad.then(() => setLoaded(true));
-  }, [loaded]);
-
-  const svg = useMemo(() => {
-    if (!loaded) return "";
-    let prev = Date.now();
-    const result = render(variables);
-    console.log(
-      "svg calc time = " +
-      (Date.now() - prev) +
-      "ms – " +
-      (result.length / (1024 * 1024)).toFixed(3) +
-      " Mb"
-    );
-    const props = inferProps(result);
-    setFeatures(props);
-    return result;
-  }, [variables.opts, loaded]);
-
-  const palette = useMemo(() => svg && inferPalette(svg), [svg]);
-
-  const renderedSVG = useMemo(
-    () =>
-      "data:image/svg+xml;base64," +
-      btoa(svg.replace(svgMMSize[1], heightPx).replace(svgMMSize[0], widthPx)),
-    [svg, widthPx, heightPx]
-  );
-
-  if (!svg || !palette) return null;
-
-  return (
-    <div
-      style={{
-        width,
-        height,
-        position: "relative",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <div style={{ position: "relative", width: W, height: H }}>
-        <div
-          style={{
-            zIndex: 1,
-            position: "relative",
-            pointerEvents: "none",
-            background: "white",
-          }}
-        >
-          <Surface
-            width={W}
-            height={H}
-            webglContextAttributes={{
-              preserveDrawingBuffer: true,
-            }}
-          >
-            <LinearCopy>
-              <Post
-                ready={!!svg}
-                size={{ width: w, height: h }}
-                palette={palette}
-              >
-                {renderedSVG}
-              </Post>
-            </LinearCopy>
-          </Surface>
-        </div>
-        <Downloadable svg={svg} palette={palette} />
-      </div>
-    </div>
-  );
-};
-
-const dlStyle = {
-  opacity: 0,
-  width: "100%",
-  height: "100%",
-  zIndex: 0,
-  position: "absolute",
-  top: 0,
-  left: 0,
-};
-function Downloadable({ svg, palette }) {
-  const [uri, setURI] = useState(null);
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      let svgOut = svg
-        .replace("background:white", `background:${palette.paper[1]}`)
-        .replace(/opacity="[^"]*"/g, 'style="mix-blend-mode: multiply"');
-
-      svgOut = svgOut
-        .replace(/#0FF/g, palette.primary[1])
-        .replace(/#F0F/g, palette.secondary[1])
-        .replace(/#FF0/g, palette.third[1]);
-
-      setURI("data:image/svg+xml;base64," + btoa(svgOut));
-    }, 500);
-    return () => clearTimeout(timeout);
-  }, [svg, palette]);
-  return <img style={dlStyle} src={uri} />;
-}
-
-function useVariables({ hash, }) {
-  return useMemo(() => generateVariables(hash,), [hash]);
-}
-
-const paperSeed = 10 * Math.random();
-
-const Paper = ({ grain }) => (
-  <Node
-    shader={shaders.paper}
-    uniforms={{ seed: paperSeed, grain, resolution: Uniform.Resolution }}
-  />
-);
-
-const PaperCache = React.memo(Paper);
-
-function useTime(ready, fps = 60) {
-  const [time, setTime] = useState(0);
-  useEffect(() => {
-    if (!ready) return;
-    let startT;
-    let h;
-    function loop(t) {
-      h = requestAnimationFrame(loop);
-      if (!startT) startT = t;
-      setTime(Math.floor((fps * (t - startT)) / 1000));
+  const props = {};
+  const _props = JSON.parse(svg.match("data-traits='([^']+)'")[1]);
+  for (let k in _props) {
+    if (_props[k]) {
+      props[camelCaseFeature(k)] = _props[k];
     }
-    h = requestAnimationFrame(loop);
-    return () => cancelAnimationFrame(h);
-  }, [ready]);
-  return time;
-}
+  }
+  $fx.features(props);
 
-function useColorRGB(str) {
-  return useMemo(() => {
-    const c = new Color(str);
-    return c
-      .rgb()
-      .array()
-      .map((x) => x / 255);
-  }, [str]);
-}
+  const effectsHot = svg.match("data-effects-hot='([^']+)'")[1];
+  const effectsWater = svg.match("data-effects-water='([^']+)'")[1];
 
-const Post = ({ ready, size, children, palette }) => {
-  const time = useTime(ready, 30) / 30;
+  const effectsHotImg = new Image();
+  effectsHotImg.src = effectsHot;
+  const effectsWaterImg = new Image();
+  effectsWaterImg.src = effectsWater;
+
+
+  if (debug) {
+    /*
+    document.body.appendChild(effectsHotImg);
+    document.body.appendChild(effectsWaterImg);
+    */
+  }
+
+  // Generate the WebGL
+
+  let MAX = 4096;
+  let ratio = width / height;
+  let paperSeed = $fx.rand() * 999;
+  let { ceil, min, max } = Math;
+  let WINDOW = window;
+  let LINEAR = "linear";
+  let PX = (px) => px + "px";
+  let MM = (mm) => mm + "mm";
+  let TRIANGLE = [-2, 0, 0, -2, 2, 2];
+  let DOC = document;
+  let BODY = DOC.body;
+  let assign = Object.assign;
+  let ABSOLUTE = "absolute";
+  let CENTER = "center";
+  let HUNDREDPC = "100%";
+  let sharedStyle = { position: ABSOLUTE, opacity: 0 };
+
+  let createElement = (e) => DOC.createElement(e);
+  let append = (n, e) => n.appendChild(e);
+  let appendBody = (e) => append(BODY, e);
+  let makeSVGDataImage = (svg) => "data:image/svg+xml;base64," + btoa(svg);
+  let adaptiveSvgWidth = (width) => max(64, ceil(width / 64) * 64);
+
+  let calcSizes = (width, height) => {
+    let dpr = WINDOW.devicePixelRatio || 1;
+    let W = width;
+    let H = height;
+    H = min(H, W / ratio) | 0;
+    W = min(W, H * ratio) | 0;
+    let w = min(MAX, dpr * W);
+    let h = min(MAX, dpr * H);
+    h = min(h, w / ratio) | 0;
+    w = min(w, h * ratio) | 0;
+    let svgW = adaptiveSvgWidth(w);
+    let svgWidth = svgW;
+    let svgHeight = (svgW / ratio) | 0;
+    return [W, H, w, h, svgWidth, svgHeight];
+  };
+
+  assign(BODY.style, {
+    display: "flex",
+    alignItems: CENTER,
+    justifyContent: CENTER,
+    backgroundColor: palette.paper[1],
+  });
+
+  let container = createElement("div");
+
+  let bgImage = createElement("img");
+  bgImage.src = makeSVGDataImage(
+    svg
+      .replace("background:white", `background:${palette.paper[1]}`)
+      .replace(/opacity="[^"]*"/g, 'style="mix-blend-mode: multiply"')
+      .replace(/#0FF/g, palette.primary[1])
+      .replace(/#F0F/g, palette.secondary[1])
+      .replace(/#FF0/g, palette.third[1]),
+  );
+  assign(container.style, sharedStyle);
+  assign(bgImage.style, {
+    top: 0,
+    left: 0,
+    width: HUNDREDPC,
+    height: HUNDREDPC,
+    ...sharedStyle,
+  });
+
+  let V = ({ viewportWidth, viewportHeight }) => [
+    viewportWidth,
+    viewportHeight,
+  ];
+
+  let canvas = createElement("canvas");
+  canvas.style.pointerEvents = "none";
+
+  append(container, bgImage);
+  appendBody(container);
+  appendBody(canvas);
+
+  let regl = createREGL(canvas);
+  let prop = regl.prop;
+
+  let vert = `precision mediump float;attribute vec2 p;varying vec2 uv;void main(){uv=p;gl_Position=vec4(2.*p-1.,0,1);}`;
+
+  let framebuffer = regl.framebuffer();
+
+  let paper = regl({
+    framebuffer,
+    frag: paperGLSL.sourceCode,
+    vert,
+    attributes: {
+      p: TRIANGLE,
+    },
+    uniforms: {
+      [paperGLSL.uniforms.seed.variableName]: paperSeed,
+      [paperGLSL.uniforms.grain.variableName]: 100,
+      [paperGLSL.uniforms.resolution.variableName]: V,
+    },
+    count: 3,
+  });
 
   const isBlackPaper = palette.paper[2];
+  const g = isBlackPaper ? mainDarkGLSL : mainGLSL;
   const grainAmp = isBlackPaper ? 0.07 : 0.13;
-  const lighting = 0.15;
+  const lighting = isBlackPaper ? 0.2 : 0.05;
   const baseColor = [-0.003, -0.006, -0.01];
-  const background = useColorRGB(palette.paper[1]);
-  const primary = useColorRGB(palette.primary[1]);
-  const secondary = useColorRGB(palette.secondary[1]);
-  const third = useColorRGB(palette.third[1]);
-  const primaryHighlight = useColorRGB(palette.primary[2]);
-  const secondaryHighlight = useColorRGB(palette.secondary[2]);
-  const thirdHighlight = useColorRGB(palette.third[2]);
+  const effect = $fx.isPreview ? 0 : 1;
 
-  return (
-    <Node
-      {...size}
-      shader={isBlackPaper ? shaders.mainBlack : shaders.main}
-      uniforms={{
-        t: children,
-        paper: <PaperCache width={size.width} grain={100} />,
-        primary,
-        primaryHighlight,
-        secondary,
-        secondaryHighlight,
-        third,
-        thirdHighlight,
-        grainAmp,
-        lighting,
-        baseColor,
-        time,
-        background,
-      }}
-    />
-  );
-};
+  let render = regl({
+    frag: g.sourceCode,
+    vert,
+    attributes: {
+      p: TRIANGLE,
+    },
+    uniforms: {
+      [g.uniforms.time.variableName]: prop("T"),
+      [g.uniforms.t.variableName]: prop("t"),
+      [g.uniforms.waterT.variableName]: prop("waterT"),
+      [g.uniforms.hotT.variableName]: prop("hotT"),
+      [g.uniforms.effect.variableName]: effect,
+      [g.uniforms.paper.variableName]: framebuffer,
+      [g.uniforms.primary.variableName]: colorRgb(palette.primary[1]),
+      [g.uniforms.primaryHighlight.variableName]: colorRgb(palette.primary[2]),
+      [g.uniforms.secondary.variableName]: colorRgb(palette.secondary[1]),
+      [g.uniforms.secondaryHighlight.variableName]: colorRgb(
+        palette.secondary[2],
+      ),
+      [g.uniforms.third.variableName]: colorRgb(palette.third[1]),
+      [g.uniforms.thirdHighlight.variableName]: colorRgb(palette.third[2]),
+      [g.uniforms.grainAmp.variableName]: grainAmp,
+      [g.uniforms.lighting.variableName]: lighting,
+      [g.uniforms.baseColor.variableName]: baseColor,
+      [g.uniforms.background.variableName]: colorRgb(palette.paper[1]),
+    },
+    count: 3,
+  });
 
-const gainShader = `
-float gain = smoothstep(0.5, 1.0, abs(cos(${Math.PI}*(abs(p.y-0.5)-0.2*time))));
-`;
+  let img = createElement("img");
+  let lastPaperWidth;
+  let lastSVGWidth;
+  let txParam = (data) => ({ data, min: LINEAR, mag: LINEAR, flipY: true });
 
-const shaders = Shaders.create({
-  paper: {
-    frag: `precision highp float;
-    varying vec2 uv;
-    uniform vec2 resolution;
-    uniform float grain, seed;
-    
+  let tex = regl.texture(txParam(null));
+  let resize = (width, height) => {
+    let [W, H, w, h, svgWidth, svgHeight] = calcSizes(width, height);
+    canvas.width = w;
+    canvas.height = h;
+    container.style.width = canvas.style.width = PX(W);
+    container.style.height = canvas.style.height = PX(H);
+    if (lastPaperWidth !== w) {
+      framebuffer.resize(w, h);
+      paper();
+    }
+    if (lastSVGWidth !== svgWidth) {
+      lastSVGWidth = svgWidth;
+      img.onload = () => tex(txParam(img));
+      img.src = makeSVGDataImage(svg);
+      img.width = svgWidth;
+      img.height = svgHeight;
+    }
+  };
 
-void pR(inout vec2 p, float a) {
-  p = cos(a)*p + sin(a)*vec2(p.y, -p.x);
-}
-float hash(float p) {
-  p = fract(p * .1031);
-  p *= p + 33.33;
-  p *= p + p;
-  return fract(p);
-}
-float hash(vec2 p) {
-  vec3 p3  = fract(vec3(p.xyx) * .1031);
-  p3 += dot(p3, p3.yzx + 33.33);
-  return fract((p3.x + p3.y) * p3.z);
-}
-float noise(float x) {
-  float i = floor(x);
-  float f = fract(x);
-  float u = f * f * (3.0 - 2.0 * f);
-  return mix(hash(i), hash(i + 1.0), u);
-}
-float noise(vec2 x) {
-  vec2 i = floor(x);
-  vec2 f = fract(x);
-  float a = hash(i);
-  float b = hash(i + vec2(1.0, 0.0));
-  float c = hash(i + vec2(0.0, 1.0));
-  float d = hash(i + vec2(1.0, 1.0));
-  vec2 u = f * f * (3.0 - 2.0 * f);
-  return mix(a, b, u.x) + (c - a) * u.y * (1.0 - u.x) + (d - b) * u.x * u.y;
-}
-const mat2 m2 = mat2( 0.4,  0.7, -0.7,  0.4 );
-float fbm( in vec2 x ) {
-  float f = 2.0;
-  float s = 0.55;
-  float a = 0.0;
-  float b = 0.5;
-  for( int i=0; i<3; i++ ) {
-    float n = noise(x);
-    a += b * n;
-    b *= s;
-    x = f * x;
+  let waterT = regl.texture(txParam(null));
+  let hotT = regl.texture(txParam(null));
+  effectsHotImg.onload = () => hotT(txParam(effectsHotImg));
+  effectsWaterImg.onload = () => waterT(txParam(effectsWaterImg));
+
+  let r = (onresize = () => resize(WINDOW.innerWidth, WINDOW.innerHeight));
+  r();
+
+  let startT;
+  regl.frame(({ time }) => {
+    if (!startT) startT = time;
+    let T = time - startT;
+    render({ T, t: tex, waterT, hotT });
+  });
+
+  // global helpers
+  function camelCaseFeature(key) {
+    let keyInCamelCase = "";
+    let shouldUppercase = true;
+    for (let i = 0; i < key.length; i++) {
+      const c = key[i];
+      if (shouldUppercase) {
+        keyInCamelCase += c.toUpperCase();
+        shouldUppercase = false;
+      } else if (c === "_") {
+        shouldUppercase = true;
+        keyInCamelCase += " ";
+      } else {
+        keyInCamelCase += c;
+      }
+    }
+    return keyInCamelCase;
   }
-  return a;
-}
-float paper(vec2 p, float z, float seed) {
-  pR(p, seed);
-  p += seed;
-  float n = 0.7 * smoothstep(-0.1, 0.2, abs(fbm(-3.0 * p * z - seed)-0.5)-0.01) +
-  0.3 * smoothstep(0.0, 0.1, abs(fbm(-6.0 * p * z + seed)-0.5)-0.01);
-  return n;
-}
 
-void main () {
-  vec2 ratio = resolution / min(resolution.x, resolution.y);
-  vec2 p = 0.5 + (uv - 0.5) * ratio;
-  float t = paper(p, grain, seed);
-  gl_FragColor = vec4(t, t, t, 1.0);
-}`,
-  },
-  main: {
-    frag: GLSL`
-    precision highp float;
-    varying vec2 uv;
-    uniform vec3 baseColor, background;
-    uniform float grainAmp, lighting, time;
-    uniform vec3 primary, primaryHighlight, secondary, secondaryHighlight, third, thirdHighlight;
-    uniform sampler2D t, paper;
-    vec3 pal(float t, vec3 c1, vec3 c2){
-      float m = smoothstep(0.3, 0.0, t);
-      return mix(
-        background,
-        mix(c1, c2, m),
-        smoothstep(1.0, 0.5, t)
-      );
-    } 
-    void main() {
-      vec2 p = uv;
-      ${gainShader}
-      vec4 g = texture2D(paper, p);
-      float grain = g.r;
-      vec4 v = texture2D(t, p);
-      vec3 c1 = pal(v.r, primary, primaryHighlight);
-      vec3 c2 = pal(v.g, secondary, secondaryHighlight);
-      vec3 c3 = pal(v.b, third, thirdHighlight);
-      vec3 c =
-        min(vec3(1.), c1 * c2 * c3 * (1. + lighting * gain)) +
-        grainAmp * /*developed by @greweb*/
-        mix(1.0, 0.5, step(0.5, grain)) *
-        (0.5 - grain) +
-        baseColor;
-      gl_FragColor = vec4(c, 1.0);
+  function colorRgb(str) {
+    let r, g, b;
+
+    // Check for #rgb or #rrggbb format
+    if (str[0] === "#") {
+      if (str.length === 4) {
+        // Convert #rgb to #rrggbb
+        str = "#" + str[1] + str[1] + str[2] + str[2] + str[3] + str[3];
+      }
+      r = parseInt(str.substr(1, 2), 16);
+      g = parseInt(str.substr(3, 2), 16);
+      b = parseInt(str.substr(5, 2), 16);
     }
-  `,
-  },
-  mainBlack: {
-    frag: GLSL`
-    precision highp float;
-    varying vec2 uv;
-    uniform vec3 baseColor, background;
-    uniform float grainAmp, lighting, time;
-    uniform vec3 primary, primaryHighlight, secondary, secondaryHighlight, third, thirdHighlight;
-    uniform sampler2D t, paper;
-    vec3 pal(float t, vec3 c1, vec3 c2) {
-      float m = smoothstep(0.3, 0.0, t);
-      return mix(
-        vec3(0.0),
-        mix(c1, c2, m),
-        smoothstep(1.0, 0.5, t)
-      );
-    } 
-    void main() {
-      vec2 p = uv;
-      ${gainShader}
-      vec4 g = texture2D(paper, p);
-      float grain = g.r;
-      vec4 v = texture2D(t, p);
-      vec3 c1 = pal(v.r, primary, primaryHighlight);
-      vec3 c2 = pal(v.g, secondary, secondaryHighlight);
-      vec3 c3 = pal(v.b, third, thirdHighlight);
-      vec3 c =
-      (c1 + c2 + c3) * (1. + lighting * gain) +
-        grainAmp * grain +/*developed by @greweb*/
-        baseColor +
-        background * smoothstep(0.5, 1.0, v.r * v.g);
-      gl_FragColor = vec4(c, 1.0);
+    // Check for rgb(r, g, b) format
+    else if (str.startsWith("rgb(")) {
+      const parts = str.match(/rgb\((\d+),\s*(\d+),\s*(\d+)\)/);
+      [r, g, b] = parts.slice(1, 4).map(Number);
     }
-  `,
-  },
+    // Convert to normalized RGB values
+    return [r, g, b].map((x) => x / 255);
+  }
 });
-
-export default Main;
